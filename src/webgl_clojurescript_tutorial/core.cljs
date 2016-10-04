af5890e503956ee1548b0d3dbbba86fb954c51b2
Christoph Frick <cf@ofnir.net>
2016-10-04 11:05:25 +0200

    Triangle

--- src/webgl_clojurescript_tutorial/core.cljs
+++ src/webgl_clojurescript_tutorial/core.cljs
@@ -1,12 +1,48 @@
 (ns webgl-clojurescript-tutorial.core
-  (:require [thi.ng.geom.gl.core :as gl]))
+  (:require [thi.ng.geom.gl.core :as gl]
+            [thi.ng.geom.gl.webgl.constants :as glc]
+            [thi.ng.geom.matrix :as mat]
+            [thi.ng.geom.core :as geom]
+            [thi.ng.geom.gl.glmesh :as glmesh]
+            [thi.ng.geom.gl.shaders :as shaders]
+            [thi.ng.geom.gl.camera :as cam]
+            [thi.ng.geom.triangle :as tri]
+            [thi.ng.geom.sphere :as sph]))
 
 (enable-console-print!)
 
 (defonce gl-ctx (gl/gl-context "main"))
 
+(def triangle
+  (geom/as-mesh (tri/triangle3 [[1 0 0] [0 0 0] [0 1 0]])
+                {:mesh (glmesh/gl-mesh 3)}))
+
+(def shader-spec
+  {:vs "void main() {
+       gl_Position = proj * view * vec4 (position, 1.0);
+       }"
+   :fs "void main() {
+       gl_FragColor = vec4(0.5,0.5,1.0,1.0);
+       }"
+   :uniforms {:view :mat4
+              :proj :mat4}
+   :attribs {:position :vec3}})
+
+(defonce camera (cam/perspective-camera {}))
+
+#_(println camera)
+
+(defn combine-model-shader-camera
+  [model shader-spec camera]
+  (-> model
+      (gl/as-gl-buffer-spec {})
+      (assoc :shader (shaders/make-shader-from-spec gl-ctx shader-spec))
+      (gl/make-buffers-in-spec gl-ctx glc/static-draw)
+      (cam/apply camera)))
+
 (doto gl-ctx
-  (gl/clear-color-and-depth-buffer 0 0 0 1 1))
+  (gl/clear-color-and-depth-buffer 0 0 0 1 1)
+  (gl/draw-with-shader (combine-model-shader-camera triangle shader-spec camera)))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
